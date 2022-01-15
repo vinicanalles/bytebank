1de8bb2923c49ff3d5a0261d4bcd98cb3282197b
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-15 14:32:17 -0300

    Criando classes para cada funcionário - Criação da classe Gerente.kt

--- bytebank/src/main/kotlin/Funcionario.kt
+++ bytebank/src/main/kotlin/Funcionario.kt
@@ -1,20 +1,9 @@
 class Funcionario(
     val nome: String,
     val cpf: String,
-    val salario: Double,
-    val tipo: Int //0 funcionário, 1 gerente, 2 diretor
+    val salario: Double
 ) {
     fun bonificacao(): Double {
-        return when (tipo) {
-            0 -> {
-                salario * 0.1
-            }
-            1 -> {
-                salario * 0.2
-            }
-            else -> {
-                salario * 0.3
-            }
-        }
+        return salario * 0.1
     }
 }
--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,12 +1,23 @@
 fun main() {
     println("Bem vindo ao ByteBank!")
 
-    val alex = Funcionario(nome = "Alex", cpf = "111.111.111-11", salario = 1000.0, tipo = 0)
+    val alex = Funcionario(nome = "Alex", cpf = "111.111.111-11", salario = 1000.0)
 
     println("Nome: ${alex.nome}")
     println("CPF: ${alex.cpf}")
     println("Salário: ${alex.salario}")
-    println("Tipo: ${alex.tipo}")
-
     println("Bonificação do ${alex.nome}: ${alex.bonificacao()}")
+
+    val fran = Gerente(nome = "Fran", cpf = "222.222.222-22", salario = 2000.0, senha = 1234)
+
+    println("Nome: ${fran.nome}")
+    println("CPF: ${fran.cpf}")
+    println("Salário: ${fran.salario}")
+    println("Bonificacao da ${fran.nome}: ${fran.bonificacao()}")
+
+    if (fran.autentica(senha = 1239)){
+        println("Gerente autenticado com sucesso!")
+    }else {
+        println("Falha na autenticação!")
+    }
 }
