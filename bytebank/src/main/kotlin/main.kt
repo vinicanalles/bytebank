150342570a9d00da19b7458bcff26113ebdeaefe
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-16 14:35:43 -0300

    Utilizando abstrações

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,35 +1,4 @@
 fun main() {
     println("Bem vindo ao ByteBank!")
-
-    val contaCorrente = ContaCorrente(
-        titular = "Alex",
-        numero = 1000
-    )
-
-    val contaPoupanca = ContaPoupanca(
-        titular = "Fran",
-        numero = 1001
-    )
-
-    contaCorrente.deposita(1000.0)
-    contaPoupanca.deposita(1000.0)
-
-    println("Saldo contaCorrente: ${contaCorrente.saldo}")
-    println("Saldo contaPoupança: ${contaPoupanca.saldo}")
-
-    contaCorrente.sacar(100.0)
-    contaPoupanca.sacar(100.0)
-
-    println("Saldo contaCorrente após saque: ${contaCorrente.saldo}")
-    println("Saldo contaPoupança após saque: ${contaPoupanca.saldo}")
-
-    contaCorrente.transfere(destino = contaPoupanca, valor = 100.0)
-
-    println("Saldo contaCorrente após transferir para poupança: ${contaCorrente.saldo}")
-    println("Saldo contaPoupança após receber transferência: ${contaPoupanca.saldo}")
-
-    contaPoupanca.transfere(destino = contaCorrente, valor = 200.0)
-
-    println("Saldo contaCorrente após receber transferência: ${contaCorrente.saldo}")
-    println("Saldo contaPoupança após transferência para corrente: ${contaPoupanca.saldo}")
+    testaFuncionarios()
 }
