bafab6d1be068164f90a983e345f8427b5ae4245
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-16 22:18:24 -0300

    Problema de reutilizar código de herança

--- bytebank/src/main/kotlin/main.kt
+++ bytebank/src/main/kotlin/main.kt
@@ -1,25 +1,3 @@
 fun main() {
     testaFuncionarios()
 }
-
-fun testaAutenticacao() {
-    val gerente = Gerente(
-        nome = "Alex",
-        cpf = "111.111.111-11",
-        salario = 1000.0,
-        senha = 1000
-    )
-
-    val diretora = Diretor(
-        nome = "Fran",
-        cpf = "222.222.222-22",
-        salario = 2000.0,
-        senha = 2000,
-        plr = 200.0
-    )
-
-    val sistemaInterno = SistemaInterno()
-
-    sistemaInterno.entra(gerente, senha = 2000)
-    sistemaInterno.entra(diretora, senha = 1000)
-}
