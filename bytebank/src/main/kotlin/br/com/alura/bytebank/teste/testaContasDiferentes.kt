c744036c9ecee219f741f762a27ea5483e02e92a
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-23 10:48:53 -0300

    Criando o endereço do cliente

--- bytebank/src/main/kotlin/br/com/alura/bytebank/teste/testaContasDiferentes.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/teste/testaContasDiferentes.kt
@@ -1,16 +1,20 @@
 import br.com.alura.bytebank.modelo.Cliente
 import br.com.alura.bytebank.modelo.ContaCorrente
 import br.com.alura.bytebank.modelo.ContaPoupanca
+import br.com.alura.bytebank.modelo.Endereco
 
 fun testaContasDiferentes() {
     val contaCorrente = ContaCorrente(
-        titular = Cliente(nome = "Alex", cpf = "", senha = 1),
+        titular = Cliente(nome = "Alex", cpf = "", senha = 1, endereco = Endereco(
+            logradouro = "Rua Vergueiro"
+        )),
         numero = 1000
     )
 
     println("titular ${contaCorrente.titular}")
     println("nome do titular ${contaCorrente.titular.nome}")
     println("cpf do titular ${contaCorrente.titular.cpf}")
+    println("logradouro ${contaCorrente.titular.endereco.logradouro}")
 
     val contaPoupanca = ContaPoupanca(
         titular = Cliente(nome = "Fran", cpf = "", senha = 2),
