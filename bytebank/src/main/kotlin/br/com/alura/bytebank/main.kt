27f9afde4f8c946b4506734b235c72a29ddaae67
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-23 11:31:37 -0300

    Conhecendo o Object Expressions

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,9 +1,19 @@
-import br.com.alura.bytebank.modelo.Cliente
-import br.com.alura.bytebank.modelo.ContaCorrente
-import br.com.alura.bytebank.modelo.ContaPoupanca
-import br.com.alura.bytebank.modelo.totalContas
+import br.com.alura.bytebank.modelo.*
 
 fun main() {
+
+    val fran = object : Autenticavel {
+        val nome: String = "Fran"
+        val cpf: String = "111.111.111-11"
+        val senha: Int = 1000
+
+        override fun autentica(senha: Int) = this.senha == senha
+    }
+
+    val sistemaInterno = SistemaInterno()
+    sistemaInterno.entra(fran, 1000)
+
+    println("Nome do cliente -> ${fran.nome}")
 
     val alex = Cliente(nome = "Alex", cpf = "", senha = 1)
     val contaPoupanca = ContaPoupanca(titular = alex, numero = 1000)
