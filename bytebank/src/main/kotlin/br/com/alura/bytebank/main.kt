d6a287c4ac0b179ce4262cebb0bfba7b569fa3fd
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-23 14:33:32 -0300

    Conhecendo e sobrescrevendo os métodos do Any

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,5 +1,21 @@
-import br.com.alura.bytebank.teste.testaAutenticacaoTiposDiferentes
+import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    testaAutenticacaoTiposDiferentes()
+
+    val endereco = Endereco(logradouro = "Rua Vergueiro", complemento = "Alura")
+    val enderecoNovo = Endereco(bairro = "Vila Mariana", numero = 1000)
+
+    println(endereco.equals(endereco))
+
+    println(endereco.hashCode())
+    println(enderecoNovo.hashCode())
+
+    println(endereco)
+    println(enderecoNovo)
+
+    println(
+        "${endereco.javaClass}@${
+            Integer.toHexString(endereco.hashCode())
+        }"
+    )
 }
