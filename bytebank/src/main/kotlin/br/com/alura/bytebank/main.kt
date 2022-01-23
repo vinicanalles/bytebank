b7b6ba06193720ddbceacf4ae304bffab5e56128
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-23 14:52:15 -0300

    Sobrescrevendo o equals e o hashcode

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -2,10 +2,18 @@ import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
 
-    val endereco = Endereco(logradouro = "Rua Vergueiro", complemento = "Alura")
-    val enderecoNovo = Endereco(bairro = "Vila Mariana", numero = 1000)
+    val endereco = Endereco(
+        logradouro = "Rua Vergueiro",
+        complemento = "Alura",
+        cep = "00000-000"
+    )
+    val enderecoNovo = Endereco(
+        logradouro = "Rua Vergueiro",
+        complemento = "Alura",
+        cep = "00000-000"
+    )
 
-    println(endereco.equals(endereco))
+    println(endereco.equals(enderecoNovo))
 
     println(endereco.hashCode())
     println(enderecoNovo.hashCode())
