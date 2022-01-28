f98265c240b979a6226fc5eef51834817d1c6159
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-28 08:39:48 -0300

    Utilizando Safe Call

--- bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/bytebank/main.kt
@@ -1,7 +1,10 @@
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    var enderecoNulo: Endereco? = null
-    val enderecoNaoNulo: Endereco = enderecoNulo!!
-    enderecoNaoNulo.logradouro
+    var enderecoNulo: Endereco? = Endereco(logradouro = "Rua Vergueiro")
+    val logradouroNovo: String? = enderecoNulo?.logradouro
+
+    enderecoNulo?.let { endereco ->
+        println(endereco.logradouro.length)
+    }
 }
