e424ac143aa9098262befff848dca0e4a5c25f5e
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-29 12:19:53 -0300

    Alterando um elemento

--- arrays/src/main/kotlin/IntroArrays.kt
+++ arrays/src/main/kotlin/IntroArrays.kt
@@ -1,21 +1,5 @@
+import teste.testaAlteracaoElementosArray
+
 fun main() {
-    val idades: IntArray = intArrayOf(25, 19, 33, 20, 55, 40)
-
-    var maiorIdade = Int.MIN_VALUE
-    for (idade in idades) {
-        if (idade > maiorIdade) {
-            maiorIdade = idade
-        }
-    }
-
-    println(maiorIdade)
-
-    var menorIdade = Int.MAX_VALUE
-    idades.forEach { idade ->
-        if (idade < menorIdade) {
-            menorIdade = idade
-        }
-    }
-
-    println(menorIdade)
+    testaAlteracaoElementosArray()
 }
