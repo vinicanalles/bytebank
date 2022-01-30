dd531cdd636b4d05790e6cbc8cc4d3f5c1b3dd40
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-01-30 11:58:05 -0300

    Trabalhando com listas

---
+++ arrays/src/main/kotlin/lists/ListaDeLivros.kt
@@ -0,0 +1,45 @@
+package lists
+
+fun main() {
+    val livro1 = Livro(
+        titulo = "Grande Sertão: Veredas",
+        autor = "João Guimarães Rosa",
+        anoPublicacao = 1956
+    )
+
+    val livro2 = Livro(
+        titulo = "Minha vida de menina",
+        autor = "Helena Morley",
+        anoPublicacao = 1942,
+        editora = "Editora A"
+    )
+
+    val livro3 = Livro(
+        titulo = "Memórias Póstumas de Brás Cubas",
+        autor = "Machado de Assis",
+        anoPublicacao = 1881
+    )
+
+    val livro4 = Livro(
+        titulo = "Iracema",
+        autor = "José de Alencar",
+        anoPublicacao = 1865,
+        editora = "Editora B"
+    )
+
+    val livros: MutableList<Livro> = mutableListOf(livro1, livro2, livro3, livro4)
+
+    livros.add(
+        Livro(
+            titulo = "Sagarana",
+            autor = "João Guimarães Rosa",
+            anoPublicacao = 1946
+        )
+    )
+
+    println(livros.size)
+
+    livros.remove(livro1)
+
+    println(livros.size)
+}
---
+++ arrays/src/main/kotlin/lists/Livro.kt
@@ -0,0 +1,8 @@
+package lists
+
+data class Livro(
+    val titulo: String,
+    val autor: String,
+    val anoPublicacao: Long,
+    val editora: String? = null
+)
