4713a831373efcf1fc9ae5a33e8b503f8dface3a
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-02-01 08:54:40 -0300

    Tipos de Coleção

--- kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -1,7 +1,27 @@
 package br.com.alura
 
 fun main() {
-    val nomes: Collection<String> = listOf(
+    val banco = BancoDeNomes()
+    banco.salva("Alex")
+    println(banco.nomes)
+    println(BancoDeNomes().nomes)
+}
+
+class BancoDeNomes {
+
+    val nomes: Collection<String> get() = dados
+
+    fun salva(nome: String) {
+        dados.add(nome)
+    }
+
+    companion object {
+        private val dados = mutableListOf<String>()
+    }
+}
+
+fun testaColecao() {
+    val nomes: Collection<String> = mutableListOf(
         "Alex",
         "Fran",
         "Gui",
@@ -12,7 +32,7 @@ fun main() {
     for (nome in nomes.iterator()) {
         println(nome)
     }
-
+    println(nomes)
     println("Tem o nome Alex? ${nomes.contains("Vinicius")}")
     println("Tamanho da coleção: ${nomes.size}")
 }
