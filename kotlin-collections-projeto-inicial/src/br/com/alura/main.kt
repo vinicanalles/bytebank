6df1fc4d382ee26774a1356b63bfd13f3c4f32bd
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-02-01 21:49:22 -0300

    Utilizando o Set

--- kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -1,10 +1,11 @@
 package br.com.alura
 
 fun main() {
-    val assistiramCursoAndroid = listOf("Alex", "Fran", "Gui", "Maria")
-    val assistiramCursoKotlin = listOf("Alex", "Paulo", "Maria")
-    val assistiramAmbos = mutableListOf<String>()
+    val assistiramCursoAndroid: Set<String> = setOf("Alex", "Fran", "Gui", "Maria")
+    val assistiramCursoKotlin: Set<String> = setOf("Alex", "Paulo", "Maria")
+    val assistiramAmbos = mutableSetOf<String>()
     assistiramAmbos.addAll(assistiramCursoAndroid)
     assistiramAmbos.addAll(assistiramCursoKotlin)
-    println(assistiramAmbos.distinct())
+    assistiramAmbos.add("Ana")
+    println(assistiramAmbos)
 }
