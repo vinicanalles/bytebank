182ac2c0b773bfe3d10a54ea960efd3fd96da041
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-02-01 21:22:44 -0300

    Copiando coleções

--- kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -2,14 +2,15 @@ package br.com.alura
 
 fun main() {
     val banco = BancoDeNomes()
+    val nomesSalvos: Collection<String> = banco.nomes
     banco.salva("Alex")
-    println(banco.nomes)
+    println(nomesSalvos)
     println(BancoDeNomes().nomes)
 }
 
 class BancoDeNomes {
 
-    val nomes: Collection<String> get() = dados
+    val nomes: Collection<String> get() = dados.toList()
 
     fun salva(nome: String) {
         dados.add(nome)
