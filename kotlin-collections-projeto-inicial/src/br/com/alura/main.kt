6e2c55bda197fb1849d0fcc55e407f858b4e442d
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-02-01 21:59:36 -0300

    Comportamentos do Set

--- kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -8,4 +8,20 @@ fun main() {
     assistiramAmbos.addAll(assistiramCursoKotlin)
     assistiramAmbos.add("Ana")
     println(assistiramAmbos)
+
+    //Função Union
+    println(assistiramCursoAndroid + assistiramCursoKotlin)
+    println(assistiramCursoAndroid union assistiramCursoKotlin)
+
+    //Função Subtract
+    println(assistiramCursoKotlin - assistiramCursoAndroid)
+    println(assistiramCursoKotlin subtract assistiramCursoAndroid)
+
+    //Função Intersect
+    println(assistiramCursoKotlin intersect assistiramCursoAndroid)
+
+    val assistiramList = assistiramAmbos.toMutableList()
+    assistiramList.add("Alex")
+    println(assistiramList)
+    println(assistiramList.toSet())
 }
