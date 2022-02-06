b506972b1b4b9fdc9de2e2d196ff3b8a6838a8d4
Vinicius Reia Canalles <vicanalles@gmail.com>
2022-02-06 10:30:47 -0300

    Comportamentos de escrita do Map

--- kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
+++ kotlin-collections-projeto-inicial/src/br/com/alura/main.kt
@@ -1,7 +1,11 @@
 package br.com.alura
 
 fun main() {
-    val pedidos = mapOf(Pair(1, 20.0), Pair(2, 34.0), 3 to 50.0)
+    val pedidos: MutableMap<Int, Double> = mutableMapOf(
+        Pair(1, 20.0),
+        Pair(2, 34.0),
+        3 to 50.0
+    )
     println(pedidos)
 
     val pedido = pedidos[1]
@@ -9,8 +13,29 @@ fun main() {
         println("pedido $it")
     }
 
-    for (pedido: Map.Entry<Int, Double> in pedidos) {
-        println("Número do pedido: ${pedido.key}")
-        println("Valor do pedido: ${pedido.value}")
+    for (p: Map.Entry<Int, Double> in pedidos) {
+        println("Número do pedido: ${p.key}")
+        println("Valor do pedido: ${p.value}")
     }
+
+    pedidos[4] = 70.0
+    println(pedidos)
+
+    pedidos.put(5, 80.0)
+    println(pedidos)
+
+    pedidos[1] = 100.0
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 200.0)
+    println(pedidos)
+
+    pedidos.putIfAbsent(6, 300.0)
+    println(pedidos)
+
+    pedidos.remove(6)
+    println(pedidos)
+
+    pedidos.remove(3, 50.0)
+    println(pedidos)
 }
